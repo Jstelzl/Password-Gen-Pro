e784042d653c4e809d0305bfda1f0cffcfd86385
[josiah stelzl] <[josiahstelzl@gmail.com]>
2022-08-22 12:04:58 -0400

    added length criteria

--- assets/script.js
+++ assets/script.js
@@ -5,45 +5,73 @@ function generatePassword() {
     console.log("clicked button.")
 
     // 1. prompt user for password criteria
-    var passwordLen = window.alert("Passowrd must consist of between 8 and 128 character. It must contain at least 1 upper case letter, 1 lowercase letter, special characters, and numaric values.");
-    window.prompt("Enter length of password using a number, 8 - 128.");
-
-        // a. password length must be 8 - 128
-    if(passwordLen == "") {
+    var passwordLen = window.prompt("Enter length of password using a number, 8 - 128.");
+        // a. prompt can't be blank
+    if(passwordLen === "") {
         window.alert("Must enter content, cannot leave blank!");
-        console.log("test");
         return generatePassword();
     }
+        // b. Password must be at or above 8 characters
     else if(passwordLen < 8) {
-        window.alert("Password must be more than 8 character! ");
-        
+        window.alert("Password must not be less than 8 character! ");
         return generatePassword();
     }
+        // c. Password must be at or below 128 characters
     else if(passwordLen > 128) {
-        window.alert("Password must not have less than 128 character! ");
-        return generatePassword();
+        window.alert("Password must not have more than 128 character! ");
     }
     
-    // b. must contain: lowercase, upercase, number, special character
-    let capitalLeters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-    window.alert("Now choose a capital letter for your password, click ok to continue. ");
-    var passwordCriteria = window.prompt("Enter an upper case letter");
-    if (passwordCriteria == capitalLeters) {
-        console.log("yay");
+        // d. asks user if they want upper case letters
+    var passwordLetter = window.prompt("Do you want a capital letter, yes or no?");
+    passwordLetter = passwordLetter.toUpperCase();
+    if (!(passwordLetter === "YES" || passwordLetter === "NO")) {
+        window.alert("Invalid response, Yes or No? ");
+        console.log(passwordLetter)
     } 
-    else {
-        window.alert(" Password criteria not met, try again! ");
-        console.log('fail');
-        
-    }  
-     // 2. validate input
+        // e. asks user if they want special characters
+    var passwordCharacters = window.prompt("Do you want special characters in your password?");
+    passwordCharacters = passwordCharacters.toUpperCase();
+    if (!(passwordCharacters === "YES" || passwordCharacters === "NO")) {
+        window.alert("Invalid response, Yes or No?");
+        console.log(passwordCharacters);
+    }
+    // f. generate password based on criteria
 
-    // 3. generate password based on criteria
+    // var numbers = "0123456789";
+    // var letterLower = "abcdefghijklmnopqrstuvwzyz";
+    // var specialChar = "!@#$%^&*()";
+    // var capitalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXWZ";
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = passwordLen;
+    var password = "";
+   
+    //const criteria = [numbers, letterLower, specialChar, capitalLetter];
+    for (var i = 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber +1);
+        console.log(password);
+    }
+    document.getElementById("password").value = password;
+    console.log("runs")
 
-    // 4. display password to page
+    // if statement with condition of choices
+    // I for example numbers and lowercase
+    // Inside the if staetment combine the numbers and the lower letter variables 
+    // also inside the if statement make a for loop to randomly select from the new characters string var 
+    // var passwordChriteria = [numbers, letterLower, specialChar, capitalLetter, password];
+   
+    // for (lowerCase = 'yes' && numbers === 'yes' && special === 'no') {
+    //     console.log("read");
+    //     add numbers and lowercase into one variable
+    //     create a for loop to randomly select from that new variable
+    // }
 
-    return "Your secure password";
-}
+
+
+    // g. display password to page
+
+    return password;
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -52,7 +80,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
